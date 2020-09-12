58ac299563abf2f1d8c8db33cddabf5f7c6c3bcd
bogpetre <bogpetre@gmail.com>
2020-09-12 17:22:21 -0600

    batchref test files

--- Testing/norm_matlab.m
+++ Testing/norm_matlab.m
@@ -1,5 +1,4 @@
-%addpath('/users/jfortin/ComBatHarmonization/Matlab/scripts')
-addpath('/users/fortinj2/ComBatHarmonization/Matlab/scripts')
+addpath('/projects/bope9760/software/combat/ComBatHarmonization/Matlab/scripts')
 data =  csvread('inputData/testdata.csv',1);
 batch = [1 1 1 1 1 2 2 2 2 2];
 mod = [1 1 1 1 1 1 1 1 1 1 ;2 1 2 1 2 1 2 1 2 1]';
@@ -12,6 +11,12 @@ norm = combat(data,batch,mod,1);
 dlmwrite('outputData/testdata_combat_parametric_unadjusted_matlab.csv',norm,'delimiter',',','precision','%.14f');
 norm = combat(data,batch,mod,0);
 dlmwrite('outputData/testdata_combat_nonparametric_unadjusted_matlab.csv',norm,'delimiter',',','precision','%.14f');
+
+% reference batch test
+norm = combat(data,batch,mod,1,'ref',1);
+dlmwrite('outputData/testdata_combat_parametric_unadjusted_batchref_matlab.csv',norm,'delimiter',',','precision','%.14f');
+norm = combat(data,batch,mod,0,'ref',1);
+dlmwrite('outputData/testdata_combat_nonparametric_unadjusted_batchref_matlab.csv',norm,'delimiter',',','precision','%.14f');
 
 % Those examples are supposed to give errors:
 %mod = [1 1 1 1 1 1 1 1 1 1 ;1 2 1 2 1 2 1 2 1 2; 1 1 1 1 1 2 2 2 2 2]';
